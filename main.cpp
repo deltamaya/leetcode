42387b17713cfb46fd7e199a14d0b4c6e441b1d0
Deltamaya <deltamaya@163.com>
2023-04-12 12:38:15 +0800

    leetcode 151.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+string reverseWords(string s) {
+	reverse(s.begin(),s.end());
+	int fast,slow;fast=slow=0;
+	while(fast<s.size()){
+		while(fast<s.size()&&s[fast]==' '){
+			++fast;
+		}
+		if(slow&&fast!=s.size())s[slow++]=' ';
+		int tmp=slow;
+		while(fast<s.size()&&s[fast]!=' ') {
+			s[ slow++ ] = s[ fast++ ];
+		}
+		reverse(s.begin()+tmp,s.begin()+slow);
+	}
+	s.resize(slow);
+	return s;
+}
 int main () {
-	std::cout << "Hello, World!" << std::endl;
+	string s;
+	getline(cin,s);
+	cout<<reverseWords(s);
 	return 0;
 }
