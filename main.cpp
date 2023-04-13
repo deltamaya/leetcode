403ac463d6a046332f658b0583e4b40cda59ad3b
Deltamaya <deltamaya@163.com>
2023-04-13 15:46:14 +0800

    leetcode 151.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,50 @@ string reverseWords(string s) {
 	s.resize(slow);
 	return s;
 }
+vector<int> twoSum(vector<int>& nums, int target) {
+	std::unordered_map<int, int> m;
+	for ( int i = 0; i < nums.size (); ++i ) {
+		auto iter = m.find (target - nums[ i ]);
+		if ( iter != m.end ()) {
+			return {i, iter->second};
+		}
+		m.insert ({nums[ i ], i});
+	}
+	return {};
+}
+struct cmp{
+	bool operator()(pair<int,int>a,pair<int,int>b){
+		return a.second<b.second;
+	}
+};
+vector<int> topKFrequent(vector<int>& nums, int k) {
+	unordered_map<int,int>m;
+	for(auto e:nums)m[e]++;
+	priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>s;
+	for(auto iter=m.begin();iter!=m.end();++iter){
+		s.push({iter->first,iter->second});
+	}
+	vector<int>ret;
+	while(k--){
+		ret.push_back (s.top().first);
+		s.pop();
+	}
+	return ret;
+	
+}
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+	int row=0,col=matrix[0].size()-1;
+	while(col>=0&&row<matrix.size()){
+		if(matrix[row][col]>target){
+			--col;
+		}else if(matrix[row][col]<target){
+			++row;
+		}else{
+			return true;
+		}
+	}
+	return false;
+}
 int main () {
 	string s;
 	getline(cin,s);
