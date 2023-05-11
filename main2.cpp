0036b6b5c36830ac62a3d58f3f0422402c93c781
Deltamaya <deltamaya@163.com>
2023-05-11 16:05:23 +0800

    dynamic programming

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//int main(){
-//	return 0;
+int change(int amount, vector<int>& coins) {
+	vector<int>dp(amount+1,0);
+	dp[0]=1;
+	for(auto e:coins){
+		for(int i=e;i<=amount;++i){
+			dp[i]+=dp[i-e];
+		}
+	}
+	return dp.back();
+}
+int combinationSum4(vector<int>& nums, int target) {
+	vector<int>dp(target+1,0);
+	dp[0]=1;
+	for(int i=1;i<=target;++i){
+		for(auto e:nums)
+			if(i-e>=0 && dp[i] < INT_MAX - dp[i - e])dp[i]+=dp[i-e];
+	}
+	return dp.back();
+}
+//int coinChange(vector<int>& coins, int amount) {
+//	if(!amount)return 0;
+//	vector<int>dp(amount+1,INT_MAX);
+//	dp[0]=0;
+//	for(auto coin:coins){
+//		for(int i=coin;i<=amount;++i){
+//			if(dp[i-coin]!=INT_MAX)dp[i]=min(dp[i],dp[i-coin]+1);
+//		}
+//	}
+//	if(dp.back()==INT_MAX)return -1;
+//	return dp.back();
 //}
+int numSquares(int n) {
+	vector<int>dp(n+1,INT_MAX);
+	dp[0]=0;
+	for(int i=1;i*i<=n;++i){
+		for(int j=i*i;j<=n;++j){
+			dp[j]=min(dp[j],dp[j-i*i]+1);
+		}
+	}
+	return dp.back();
+}
+bool wordBreak(string s, vector<string>& wordDict) {
+	vector<bool>dp(s.size()+1,false);
+	dp[0]=true;
+	for(int i=1;i<=s.size();++i){
+		for(auto& word:wordDict){
+			if(i>=word.size()
+			&&word==s.substr(i-word.size(),word.size())){
+				dp[i]=dp[i]||dp[i-word.size()];
+			}
+		}
+	}
+	return dp.back();
+}
+int rob(vector<int>& nums) {
+	if(nums.size()==1)return nums[0];
+//	vector<int>dp(nums.size()+1,0);
+	int dp[101]={0};
+	dp[1]=nums[0];dp[2]=nums[1];
+	for(int i=3;i<=nums.size();++i){
+		dp[i]=max(dp[i-2],dp[i-3])+nums[i-1];
+	}
+	return max(dp[nums.size()],dp[nums.size()-1]);
+}
+int main(){
+	string s="leetcode";
+	vector<string>d={"leet","cod"};
+	cout<<wordBreak(s,d);
+	return 0;
+}
